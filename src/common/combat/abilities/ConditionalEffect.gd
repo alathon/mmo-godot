339c67fc380a5fc285a7e55efa934752b85f8089
Martin Grünbaum <martin@itsolveonline.net>
2026-04-13 20:28:28 +0200

    Factor out Ability modifiers

--- src/common/combat/abilities/ConditionalEffect.gd
+++ src/common/combat/abilities/ConditionalEffect.gd
@@ -2,9 +2,4 @@ class_name ConditionalEffect
 extends Resource
 
 @export var condition: ConditionResource
-
-# Set one or the other, not both.
-# applied_modifier: applies an AbilityModifier to this cast if the condition is met.
-# added_effects: injects extra effects into this cast if the condition is met.
-@export var applied_modifier: AbilityModifier
-@export var added_effects: Array[AbilityEffect] = []
+@export var mod: AbilityMod
